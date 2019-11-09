48b98f529cd1657b47c43f138dbf51f37ef65d52
minhajspace <minhaj.space@gmail.com>
2019-11-09 16:43:08 +0530

    initial phase

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = props => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Resume </h1>
+      <table className="ui celled table">
+        <tr>
+          <td>Name : {props.name}</td>
+        </tr>
+        <tr>
+          <td>Address : {props.address}</td>
+        </tr>
+        <tr>
+          <td>Email : {props.email}</td>
+        </tr>
+        <tr>
+          <td>Contact Number : {props.number}</td>
+        </tr>
+        <th>EDUCATIONAL QUALIFICATIONS </th>
+        <tr>
+          <tr>
+            <th>Course:</th>
+            <td>{props.course}</td>
+          </tr>
+          <tr>
+            <th>Year of Passing</th>
+            <td>{props.year}</td>
+          </tr>
+          <tr>
+            <th>Passing Percentage </th>
+
+            <td>{props.percentage}</td>
+          </tr>
+          <tr>
+            <th>College/Board </th>
+
+            <td>{props.college}</td>
+          </tr>
+        </tr>
+
+        <th>TECHNICAL SKILLS </th>
+        <tr>
+          <th>Language:</th>
+          <td>{props.language}</td>
+        </tr>
+        <tr>
+          <th>Database:</th>
+          <td>{props.database}</td>
+        </tr>
+        <tr>
+          <th>Operating System:</th>
+          <td>{props.operatingSystem}</td>
+        </tr>
+        <tr>
+          <th>Tools:</th>
+          <td>{props.tools}</td>
+        </tr>
+        <tr>
+          <th>UI:</th>
+          <td>{props.ui}</td>
+        </tr>
+
+        <th>CERTIFICATION COURSES </th>
+        <tr>
+          <td>{props.cartificate}</td>
+        </tr>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = props => {
+  return (
+    <div>
+      <App
+        name="Minhaj ahmad khan "
+        email="minhaj.space@gmail.com"
+        address="lucknow"
+        course="B-tech"
+        college="Dr.MC saxena college on engg and tech "
+        interschool="Bright Career inter college Lucknow"
+        college="Dr.Mc Saxena college  of engg and tech"
+        course="B-tech"
+        language="Javascript,ReactJS "
+        database="My SQL server,MySql"
+        operatingSystem="windwos 8,windows10"
+        tools=" Visual Studio, SQL Server 2012 "
+        ui="HTML,CSS "
+        cartificate=""
+      />
+      {/* <App
+        Name="Urooj"
+        middelName=""
+        lastName="Ahmad"
+        city="Lucknow"
+        course="B-tech"
+        college="Dr.MC saxena college on engg and tech "
+        interschool="Baby martin"
+      />
+      <App
+        Name="vishal"
+        lastName="Anand"
+        city="unnao"
+        course="B-tech"
+        college="Dr.MC saxena college on engg and tech "
+        interschool="tkic"
+      /> */}
+    </div>
+  );
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
